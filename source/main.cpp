a186a9797704b4a538fc85f0051918a75a0f1bea
Alexander Wittmond <awittmond@gmail.com>
2020-10-20 12:09:16 -0500

    removed boost dependency and sigenv signal handler in main

--- source/main.cpp
+++ source/main.cpp
@@ -1,31 +1,13 @@
 
-#include <fenv.h>
-#define BOOST_STACKTRACE_USE_BACKTRACE T
 #include "simulationWindow.h"
-#include <boost/stacktrace/stacktrace_fwd.hpp>
 #include <gtkmm.h>
 #include <iostream>
 
-#include <execinfo.h>
 
-#include <boost/stacktrace.hpp>
-#include <stdexcept>
-#include <exception>
 
-void handler(int sig) {
-  std::cerr << "sigsegv called\n";
 
-  std::cerr << boost::stacktrace::stacktrace();
-  exit(1);
-}
 
 int main(int argc, char **argv) {
-  signal(SIGSEGV, handler);
-  std::set_terminate([](){
-    std::cerr << "terminate called\n";
-    std::cerr << boost::stacktrace::stacktrace();
-    std::abort();
-  });
   auto app = Gtk::Application::create(argc, argv, "kalman.sim");
 
   SimulationWindow sim;
